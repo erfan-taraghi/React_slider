629d775cf41a95ba06ce742308a580597156c621
erfan taraghi <taraghikonerfan@gmail.com>
2022-08-29 19:12:24 +0430

    added full

---
+++ src/Reviews.js
@@ -0,0 +1,62 @@
+import React,{useState} from 'react'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import data from './data'
+function Reviews() {
+
+const [index,setIndex] = useState(0);
+const{name,image,job,text} = data[index]
+
+const checkNumber= (number)=>{
+     if(number>data.length -1){
+          return 0
+     }
+     if(number<0){
+          return data.length - 1
+     }
+     return number
+}
+
+const nextSlide = () =>{
+     setIndex((index) =>{
+          let newIndex = index +1
+          return checkNumber(newIndex)
+     })
+}
+
+const prevSlide = () =>{
+     setIndex((index) =>{
+          let newIndex = index -1
+          return checkNumber(newIndex)
+     })
+}
+
+const randomPerson = () =>{
+     let randomNumber =Math.floor( Math.random() * data.length)
+     if(randomNumber === index){
+          randomNumber = index + 1
+     }
+     setIndex(checkNumber(randomNumber))
+     console.log(randomNumber)
+
+}
+
+     return (
+          <div className="reviews">
+               <div className="review-img">
+                    <img src={image} alt="" />
+               </div>
+               <h4 className="author">
+                   {name}
+               </h4>
+               <p className="job">{job}</p>
+               <p className='info'>{text}</p>
+               <div className="review-button">
+                    <button onClick={nextSlide}><FaChevronRight /></button>
+                    <button onClick={prevSlide}><FaChevronLeft /></button>
+               </div>
+               <button className="random-btn" onClick={randomPerson}>Random</button>
+          </div>
+     )
+}
+
+export default Reviews
